73e8943f2ce45cce4e1b2b29ba2200ee73c401b1
brunbs <babosa227@gmail.com>
2022-02-13 17:53:57 -0300

    Criando rota de GET para um fornecedor

--- api/rotas/fornecedores/TabelaFornecedor.js
+++ api/rotas/fornecedores/TabelaFornecedor.js
@@ -4,9 +4,23 @@ module.exports = {
     listar() {
         return Modelo.findAll();
     },
-    
+
     inserir(fornecedor) {
         return Modelo.create(fornecedor);
+    },
+
+    async pegarPorId(id) {
+        const encontrado = await Modelo.findOne({
+            where: {
+                id: id
+            }
+        });
+
+        if (!encontrado) {
+            throw new Error('Fornecedor não encontrado');
+        }
+
+        return encontrado;
     }
 
 }
--- api/rotas/fornecedores/index.js
+++ api/rotas/fornecedores/index.js
@@ -4,7 +4,7 @@ const Fornecedor = require('./Fornecedor');
 
 roteador.get('/', async (req, res) => {
     const resultados = await TabelaFornecedor.listar();
-    res.send(
+    res.status(200).send(
         JSON.stringify(resultados)
     );
 });
@@ -13,9 +13,23 @@ roteador.post('/', async (req, res) => {
     const dadosRecebidos = req.body;
     const fornecedor = new Fornecedor(dadosRecebidos);
     await fornecedor.criar();
-    res.send(
+    res.status(201).send(
         JSON.stringify(fornecedor)
     );
+});
+
+roteador.get('/:idFornecedor', async (req, res) => {
+    try {
+        const id = req.params.idFornecedor;
+        fornecedor = new Fornecedor({ id: id });
+        await fornecedor.carregar();
+        res.status(200).send(JSON.stringify(fornecedor));
+    } catch(erro) {
+        res.status(400).send(
+            JSON.stringify({
+                mensagem: erro.message
+        }))
+    }
 });
 
 module.exports = roteador;
