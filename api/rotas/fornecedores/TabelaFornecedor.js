2a71992c99c1fd1aca490f68a157ba125de3c6fa
brunbs <babosa227@gmail.com>
2022-02-13 14:57:11 -0300

    Criando rota de POST

--- api/rotas/fornecedores/TabelaFornecedor.js
+++ api/rotas/fornecedores/TabelaFornecedor.js
@@ -3,5 +3,10 @@ const Modelo = require('./modeloTabelaFornecedor');
 module.exports = {
     listar() {
         return Modelo.findAll();
+    },
+    
+    inserir(fornecedor) {
+        return Modelo.create(fornecedor);
     }
+
 }
