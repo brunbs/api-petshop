9f7809505808d9daaf87d0b7409cea8dcc8c585a
brunbs <babosa227@gmail.com>
2022-02-14 15:57:59 -0300

    Adicionando POST para produtos

---
+++ api/rotas/fornecedores/produtos/TabelaProduto.js
@@ -0,0 +1,16 @@
+const Modelo = require('./ModeloTabelaProduto');
+
+module.exports = {
+    listar(idFornecedor) {
+        return Modelo.findAll({
+            where: {
+                fornecedor: idFornecedor
+            }
+        });
+    },
+
+    inserir(dados) {
+        return Modelo.create(dados);
+    }
+
+}
--- api/rotas/fornecedores/produtos/index.js
+++ api/rotas/fornecedores/produtos/index.js
@@ -1,7 +1,22 @@
-const roteador = require('express').Router();
+const roteador = require('express').Router({ mergeParams: true });
+const Tabela = require('./TabelaProduto');
+const Produto = require('./Produto');
 
-roteador.get('/', (req, res) => {
-    res.send(JSON.stringify([]))
+roteador.get('/', async (req, res) => {
+    const produtos = await Tabela.listar(req.params.idFornecedor);
+    res.send(
+        JSON.stringify(produtos)    
+    );
 });
+
+roteador.post('/', async (req, res) => {
+    const idFornecedor = req.params.idFornecedor;
+    const corpo = req.body;
+    const dados = Object.assign({}, corpo, {fornecedor: idFornecedor});
+    const produto = new Produto(dados);
+    await produto.criar();
+    res.status(201);
+    res.send(produto);
+})
 
 module.exports = roteador;
